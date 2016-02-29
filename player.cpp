2ee6ef5846b20e0b11ee789b4a8feddcada63d5d
Sarah <asano.sar@gmail.com>
2016-02-29 10:51:44 -0800

    player.cpp returns NULL when no time is left on the clock

--- player.cpp
+++ player.cpp
@@ -35,6 +35,8 @@ Player::~Player() {
  * return NULL.
  */
 Move *Player::doMove(Move *opponentsMove, int msLeft) {
+  if (msLeft == -1)
+    { return NULL;}
     /* 
      * TODO: Implement how moves your AI should play here. You should first
      * process the opponent's opponents move before calculating your own move
